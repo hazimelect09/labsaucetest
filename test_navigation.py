977ca64c95538cd84172e140e52138532072db1f
hazimelect09 <68443613+hazimelect09@users.noreply.github.com>
2024-02-11 13:31:56 +0300

    Update test_navigation.py

--- test_navigation.py
+++ test_navigation.py
@@ -1,5 +1,4 @@
-# test_login.py
-import time
+mport time
 
 import pytest
 from playwright_base import browser, context, page
@@ -12,52 +11,44 @@ test_data_singleuser = read_test_data('data/Login_test_data.xlsx', 'valid')
 first_row_data = test_data_singleuser[0]
 @pytest.mark.parametrize("username, password", [(first_row_data['username'], first_row_data['password'])])
 def test_logout(page,username,password):
-    Log =  LoginPage(page)
-    Log.login(username,password)
-    Sidemen = Sidemenu(page)
-    Sidemen.open_sidemenu()
-    Sidemen.logout()
-    assert Sidemen.is_loggedout()
+    login_page =  LoginPage(page)
+    login_page.login(username,password)
+    sidemenu_page = Sidemenu(page)
+    sidemenu_page.open_sidemenu()
+    sidemenu_page.logout()
+    assert sidemenu_page.is_loggedout()
 @pytest.mark.parametrize("username, password", [(first_row_data['username'], first_row_data['password'])])
 def test_open_twitter(page,username,password):
-    Log =  LoginPage(page)
-    Log.login(username, password)
-    Sidemen = Sidemenu(page)
-    assert Sidemen.open_twitter()
+    login_page =  LoginPage(page)
+    login_page.login(username, password)
+    sidemenu_page = Sidemenu(page)
+    assert sidemenu_page.open_twitter()
 @pytest.mark.parametrize("username, password", [(first_row_data['username'], first_row_data['password'])])
 def test_open_linkedIn(page,username,password):
-    Log =  LoginPage(page)
-    Log.login(username, password)
-    Sidemen = Sidemenu(page)
-    assert Sidemen.open_LinkedIn()
+    login_page =  LoginPage(page)
+    login_page.login(username, password)
+    sidemenu_page = Sidemenu(page)
+    assert sidemenu_page.open_LinkedIn()
 @pytest.mark.parametrize("username, password", [(first_row_data['username'], first_row_data['password'])])
 def test_open_Facebook(page,username,password):
-    Log =  LoginPage(page)
-    Log.login(username, password)
-    Sidemen = Sidemenu(page)
-    assert Sidemen.open_Facebook()
+    login_page =  LoginPage(page)
+    login_page.login(username, password)
+    sidemenu_page = Sidemenu(page)
+    assert sidemenu_page.open_Facebook()
 
 @pytest.mark.parametrize("username, password", [(first_row_data['username'], first_row_data['password'])])
 def test_navigate_about(page,username,password):
-    Log =  LoginPage(page)
-    Log.login(username, password)
-    Sidemen = Sidemenu(page)
-    Sidemen.open_sidemenu()
-    Sidemen.open_about()
-    assert Sidemen.is_about_works()
+    login_page =  LoginPage(page)
+    login_page.login(username, password)
+    sidemenu_page = Sidemenu(page)
+    sidemenu_page.open_sidemenu()
+    sidemenu_page.open_about()
+    assert sidemenu_page.is_about_works()
 
 @pytest.mark.parametrize("username, password", [(first_row_data['username'], first_row_data['password'])])
 def test_allitem(page,username,password):
-    Log =  LoginPage(page)
-    Log.login(username,password)
-    Sidemen = Sidemenu(page)
-    Sidemen.open_sidemenu()
-    Sidemen.open_resetapp()
-
-
-
-
-
-
-
-
+    login_page =  LoginPage(page)
+    login_page.login(username,password)
+    sidemenu_page = Sidemenu(page)
+    sidemenu_page.open_sidemenu()
+    sidemenu_page.open_resetapp()
